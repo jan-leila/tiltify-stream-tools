81e834adb25050baa45db587355e65967bc3260c
Leyla <leyla@pop-os.localdomain>
2020-07-02 23:01:19 -0500

    tmp commit while going back to fix run error

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,42 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import './App.scss';
 
 import Header from './components/header';
 import SideBar from './components/side_bar';
 import Body from './components/body';
 
-// class AppWrapper extends Component {
-//   render(){
-//     return <Router>
-//       <div className='TopBar'>
-//         GFGC thing.
-//       </div>
-//       <div className='SideBar'>
-//         <Link to='/'>Home</Link><br/>
-//         <Link to='/donations'>Donations</Link><br/>
-//         <Link to='/settings'>Settings</Link><br/>
-//         <Link to='/help'>Help</Link>
-//       </div>
-//       <div className='Page'>
-//           <Route exact path='/' component={HomePage}/>
-//           <Route path='/help' component={HelpPage}/>
-//           <Route path='/donations' component={DonationPage}/>
-//           <Route path='/settings' component={SettingsPage}/>
-//       </div>
-//     </Router>;
-//   }
-// }
+// const apollo_client = new ApolloClient({
+//   uri: '/api/v1',
+// });
 
 function App() {
-  return <Router>
-    <Header/>
-    <div className="page-content">
-      <SideBar/>
-      <Body/>
-    </div>
-  </Router>
+  return (
+    // <ApolloProvider client={apollo_client}>
+      <Router>
+        <Header/>
+        <div className="page-content">
+          <SideBar/>
+          <Body/>
+        </div>
+      </Router>
+    // </ApolloProvider>
+  )
 }
 
 export default App;
--- react-ui/src/components/body/pages/sources/index.js
+++ react-ui/src/components/body/pages/sources/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 
 import './index.scss';
 
-function Page(){
+const GET_SOURCES = gql`
+  {
+    get_sources {
+      name
+      id
+    }
+  }
+`
+
+function Sources(){
+  const { loading, error, data } = useQuery(GET_SOURCES);
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  console.log(data);
   return <></>;
+}
+
+function Page(){
+
+  return <>
+    <div className='title'>Sources</div>
+    <Sources/>
+  </>;
 }
 
 export default Page;
